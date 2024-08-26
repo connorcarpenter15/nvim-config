cd3f56630c191582001a2ff5fe28c1ca55a2b81e
Connor <connorcarpenter15@gmail.com>
2024-08-26 09:31:24 -0400

    add keymaps for vimtex

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,3 +19,31 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     vim.fn.setpos(".", save_cursor)
   end,
 })
+
+-- rewrite vimtex keymaps
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "tex" },
+  callback = function()
+    vim.keymap.set("n", "<leader>vl", ":VimtexCompile<CR>")
+    vim.keymap.set("n", "<leader>vL", ":VimtexCompileSS<CR>")
+    vim.keymap.set("n", "<leader>vr", ":VimtexView<CR>")
+    vim.keymap.set("n", "<leader>vc", ":VimtexClean<CR>")
+    vim.keymap.set("n", "<leader>vC", ":VimtexCleanF<CR>")
+    vim.keymap.set("n", "<leader>vo", ":VimtexCompileOutput<CR>")
+    vim.keymap.set("n", "<leader>vk", ":VimtexStop<CR>")
+    vim.keymap.set("n", "<leader>vK", ":VimtexStopAll<CR>")
+    vim.keymap.set("n", "<leader>vt", ":VimtexTocOpen<CR>")
+    vim.keymap.set("n", "<leader>vT", ":VimtexTocToggle<CR>")
+    vim.keymap.set("n", "<leader>vi", ":VimtexInfo<CR>")
+    vim.keymap.set("n", "<leader>vI", ":VimtexInfoF<CR>")
+    vim.keymap.set("n", "<leader>vx", ":VimtexReload<CR>")
+    vim.keymap.set("n", "<leader>vX", ":VimtexReloadState<CR>")
+    vim.keymap.set("n", "<leader>vv", ":VimtexView<CR>")
+    vim.keymap.set("n", "<leader>ve", ":VimtexErrors<CR>")
+    vim.keymap.set("n", "<leader>va", ":VimtexContextMenu<CR>")
+    vim.keymap.set("n", "<leader>vg", ":VimtexStatus<CR>")
+    vim.keymap.set("n", "<leader>vq", ":VimtexLog<CR>")
+    vim.keymap.set("n", "<leader>vs", ":VimtexToggleMain<CR>")
+    vim.keymap.set("n", "<leader>vm", ":VimtexImapsList<CR>")
+  end,
+})
