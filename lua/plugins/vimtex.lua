53f781b656c01bfdea72737d6ec00af8ea1a5b07
Connor <connorcarpenter15@gmail.com>
2024-08-28 21:48:37 -0400

    switch vimtex to skim

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,5 +4,7 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_mappings_enabled = 0
+    vim.g.vimtex_view_method = "skim"
   end,
 }
